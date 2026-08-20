0b72e780c52f07dfdc09201f50159bb90ab23b21
Rafael <138343340+diasgarcia@users.noreply.github.com>
2026-08-20 01:14:23 +0000

    criando entidade categoria

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -1,6 +1,14 @@
 package br.edu.unifio.ecommerce.entidades;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Categoria {
 
-    
+    //byte short int long -- Byte Short Integer Long
+    private Short id;
+    private String nome;
+    private String descricao;
 }
