2d0d3adfa736272ba52811ddcc815660a4268240
Rafael <138343340+diasgarcia@users.noreply.github.com>
2026-09-17 01:30:40 +0000

    adicinando construtores e builds para todas as classes

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/ItemPedido.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/ItemPedido.java
@@ -7,12 +7,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder 
+@NoArgsConstructor 
+@AllArgsConstructor 
 public class ItemPedido {
 
     @Id
