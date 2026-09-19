f0c05544a9210c2366fd19e59c844bf29b721ad8
diasgarcia <rafdgar@gmail.com>
2026-09-19 00:32:20 +0000

    testes unitarios cliente

--- ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ClienteRepositorioTests.java
+++ ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ClienteRepositorioTests.java
@@ -1,11 +1,14 @@
 package br.edu.unifio.ecommerce.repositorios;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
 import br.edu.unifio.ecommerce.entidades.Cliente;
 
@@ -16,6 +19,7 @@ public class ClienteRepositorioTests {
     ClienteRepositorio clienteRepositorio;
 
     @Test
+    @Order (2)
     public void deveBuscarUmClientePorId() {
 
         Cliente cliente = clienteRepositorio.findById(2).orElseThrow();
@@ -24,4 +28,65 @@ public class ClienteRepositorioTests {
         assertEquals("Fulano", cliente.getNome());
     }
 
+    @Test 
+    @Order (1)
+    public void deveListarTodosOsClitente() {
+
+        List<Cliente> clientes = clienteRepositorio.findAll(Sort.by("nome"));
+
+        assertEquals(5, clientes.size());
+        assertEquals("Beltrano", clientes.get(1).getNome());
+        assertEquals("beltrano@email.com", clientes.get(1).getEmail());
+    }
+
+    @Test 
+    @Order (3)
+    public void deveExcluirUmClientePorId() {
+
+        Cliente cliente = Cliente.builder()
+            .nome("Cliente Teste")
+            .email("email@teste.com")
+            .telefone("0000-0000")
+            .build();
+        clienteRepositorio.save(cliente);
+
+        assertTrue(clienteRepositorio.existsById(cliente.getId()));
+        clienteRepositorio.deleteById(cliente.getId());
+        assertFalse(clienteRepositorio.existsById(cliente.getId()));
+    }
+
+    @Test 
+    @Order (4)
+    public void deveSalvarUmProduto() {
+
+        Cliente cliente = Cliente.builder()
+            .nome("Cliente Teste")
+            .email("email@teste.com")
+            .telefone("0000-0000")
+            .build();
+        clienteRepositorio.save(cliente);
+
+        assertTrue(clienteRepositorio.existsById(cliente.getId()));
+        assertEquals("Cliente Teste", clienteRepositorio.findById(cliente.getId()).orElseThrow().getNome());
+    }
+
+    @Test 
+    @Order (5)
+    public void deveAlterarUmCliete() {
+
+        Cliente cliente = Cliente.builder()
+            .nome("Cliente Teste")
+            .email("email@teste.com")
+            .telefone("0000-0000")
+            .build();
+        clienteRepositorio.save(cliente);
+
+        cliente.setNome("Cliente Alterado");
+        cliente.setEmail("alterado@email.com");
+        cliente.setTelefone("0000-0001");
+
+        assertEquals("Cliente Alterado", cliente.getNome());
+        assertEquals("alterado@email.com", cliente.getEmail());
+        assertEquals("0000-0001", cliente.getTelefone());
+    }
 }
